7cfae69a237140e90f3368d9fb11cfc9f375cd12
LiquidGangster <thomasjpkelly@gmail.com>
2018-11-27 21:53:48 -0500

    Pre testing completion.

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -1,58 +1,128 @@
-//import jdk.nashorn.api.tree.Tree;
+import jdk.nashorn.api.tree.Tree;
 
 public class BinaryTree {
 
-    private int data;
+    private TreeNode root;
     private int size;
-    private BinaryTree left;
-    private BinaryTree right;
 
-    public BinaryTree(){
-        this.data = data;
-        this.left = null;
-        this.right = null;
-    }
+    private class TreeNode {
+        private TreeNode left;
+        private TreeNode right;
+        private int data;
 
-    public int size(){
-        return this.size;
-    }
+        TreeNode(int data){
+            this.data = data;
+        }
 
-    public boolean isEmpty(){
-        return true;
-    }
-
-    public void add(int value) {
-        if (this.isEmpty()) {
-            this.data = value;
-        } else {
-            if (value < this.data) {
+        private void addTreeNode(TreeNode treeNode) {
+            if (treeNode.data< this.data) {
                 if (this.left == null) {
-                    this.left = new BinaryTree();
-                }
-                this.left.add(value);
-            } else {
+                        this.left = treeNode;
+                        }
+                        this.left.addTreeNode(treeNode);
+            } else if (treeNode.data > this.data){
                 if (this.right == null) {
-                    this.right = new BinaryTree();
+                    this.right = treeNode;
+                }else{
+                    this.right.addTreeNode(treeNode);
                 }
-                this.right.add(value);
             }
+        }
 
+        private boolean findTreeNode(int value){
+            if (value == this.data) {
+                return true;
+            } else if (value < this.data) {
+                if(this.left == null){
+                    return false;
+                }else{
+                    return this.left.findTreeNode(value);
+                }
+            } else if (value > this.data) {
+                if(this.right == null){
+                    return false;
+                }else{
+                    return this.right.findTreeNode(value);
+                }
+            }
+            return false;
+        }
+
+        private int findTreeNodeMax(TreeNode treeNode){
+            TreeNode next = treeNode;
+            while(next.right != null){
+                next = next.right;
+            }
+            return next.data;
+        }
+
+        private int findTreeNodeMin(TreeNode treeNode){
+            TreeNode next = treeNode;
+            while(next.left != null){
+                next = next.left;
+            }
+            return next.data;
         }
     }
 
-    public boolean exists(int value){
-        return true;
+
+    BinaryTree(){
+        this.root = null;
     }
 
-    public Integer max(){
-        return 0;
+//    BinaryTree(BinaryTreeNode root){
+//        this.root = root;
+//    }
+
+
+    public int size() {
+        return this.size;
     }
 
-    public Integer min(){
-        return 0;
+    public boolean isEmpty() {
+        if (this.size == 0) {
+            return true;
+        }
+        return false;
     }
 
 
+    public void add(int value){
+        TreeNode treeNode = new TreeNode(value);
+
+        if(this.root == null){
+            this.root = treeNode;
+        }else{
+            this.root.addTreeNode(treeNode);
+        }
+
+    }
+
+
+    public boolean exists(int value) {
+        return root.findTreeNode(value);
+    }
+
+    public Integer max() {
+        //handle the empty tree here
+        if(this.root == null){
+            return null;
+        }
+        return this.root.findTreeNodeMax(this.root);
+
+    }
+
+    public Integer min() {
+        if(this.root == null){
+            return null;
+        }
+        return this.root.findTreeNodeMin(this.root);
+    }
+
+
+    public static void main(String[] args){
+
+    }
 
 
 }
